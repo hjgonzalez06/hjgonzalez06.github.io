d74e73d7ec30afbe542274c2f67672a66ddae8c2
Hiram J. Gonzalez <hiramjgonzalez98@gmail.com>
2020-06-26 03:44:48 -0400

    Apply AJAX submit to the Contact form of the website

--- js/app.js
+++ js/app.js
@@ -26,6 +26,44 @@ $(document).ready(function(){
 
     });
 
-    
+    /* Envío de Formulario de Contacto con AJAX */
+    $('form').on('click','#enviar',function(e){
+        e.preventDefault();
+
+        let name = $('form').find('#name').val();
+        let email = $('form').find('#email').val();
+        let subject = $('form').find('#subject').val();
+        let message = $('form').find('#mensaje').val();
+        let action = $('form').attr('action');
+        let method = $('form').attr('method');
+
+        let data = {
+            url: action,
+            type: method,
+            dataType: 'json',
+            data: {
+                name: name,
+                email: email,
+                subject: subject,
+                message: message
+            }
+        }
+
+        function sendData(data){
+            return new Promise(function(resolve,reject){
+                $.ajax(data).done(resolve).fail(reject);
+            });
+        };
+
+        sendData(data).then(function resolve(data){
+            /* $('form input[type=text] , form textarea').each(function(){
+                this.value = '';
+            }); */
+            alert('¡Mensaje enviado exitosamente!');
+        }, function reject(reason){
+            alert('No se pudo enviar el mensaje.');
+        });
+
+    });
 
 })
